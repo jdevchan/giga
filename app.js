2a3905e1abb2938674678f28b7ac788bfe69866d
jdevchan <jdevchan@gmail.com>
2021-09-19 18:28:30 -0300

    update
    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#262626"
+        "value": "#082A36"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#262626",
+        "color": "#082A36",
         "opacity": 0.4,
         "width": 1
       },
