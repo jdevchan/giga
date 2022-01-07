7f88a8f1c96d7b1156ccb7750a48aba6c341f668
Juarez A. Rodrigues <jdevchan@gmail.com>
2022-01-07 20:50:56 -0300

    section plans

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function() {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
@@ -11,19 +11,18 @@ for (const element of toggle) {
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
 // MUDAR HEADER QUANDO DER SCROLL
 
-
 function changeHeaderWenScroll() {
   const header = document.querySelector('#header')
   const navHeight = header.offsetHeight
 
-  if (window.scrollY >= navHeight){
+  if (window.scrollY >= navHeight) {
     // SCROLL É MAIOR QUE A ALTURA DO HEADER
     header.classList.add('scroll')
   } else {
@@ -31,22 +30,6 @@ function changeHeaderWenScroll() {
     header.classList.remove('scroll')
   }
 }
-
-// TESTIMONIAL SLIDER SWIPER
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true,
-    }
-  }
-})
 
 // SCROLLREVEAL SUAVISAR EXIBIÇÃO DE ITENS
 const scrollReveal = ScrollReveal({
@@ -61,53 +44,52 @@ scrollReveal.reveal(
    #about .image, #about.text,
    #services .header, #services .card,
    #coverage .header, #coverage .map,
-   #testimonials .header, #testimonials .testimonials,
+   #plans .header, #plans,
    #contact .text, #contact .links,
    footer .brand, footer .social
-  `, {interval: 100})
+  `,
+  { interval: 100 }
+)
 
-  // BACK TO TOP
-  function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
+// BACK TO TOP
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
 
-    if(window.scrollY >= 560) {
-      backToTopButton.classList.add('show')
-    }else {
-      backToTopButton.classList.remove('show')
-    }
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
+}
 
-  // MENU ATIVO CONFORME SEÇÃO VISÍVEL NA PÁGINA
-  const sections = document.querySelectorAll('main section[id]')
-  function activateMenuAtCurrentSection() {
+// MENU ATIVO CONFORME SEÇÃO VISÍVEL NA PÁGINA
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 6
 
-    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
 
-    for( const section of sections ) {
-      const sectionTop = section.offsetTop
-      const sectionHeight = section.offsetHeight
-      const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
 
-      const checkpointStart = checkpoint >= sectionTop
-      const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-      if(checkpointStart && checkpointEnd) {
-        document
-        .querySelector('nav ul li a[href*=' + sectionId +']')
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
         .classList.add('active')
-      } else {
-        document
-        .querySelector('nav ul li a[href*=' + sectionId +']')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
         .classList.remove('active')
-      }
     }
-
   }
+}
 
-  // WHEN SCROLL
-  window.addEventListener('scroll', function() {
-    changeHeaderWenScroll()
-    backToTop()
-    activateMenuAtCurrentSection()
-  })
-
+// WHEN SCROLL
+window.addEventListener('scroll', function () {
+  changeHeaderWenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
